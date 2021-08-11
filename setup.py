0bb99cb72d0d6ac0248e11c8fb1bd641fbaba3c9
Matt Carey <matt@authorityspoke.com>
2021-08-11 04:57:58 -0500

    switch from setup.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="justopinion",
+    version="0.2.3",
+    author="Matt Carey",
+    author_email="matt@authorityspoke.com",
+    description="Download client for legal opinions",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/mscarey/justopinion",
+    project_urls={"Bug Tracker": "https://github.com/mscarey/justopinion/issues"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: Free To Use But Restricted",
+        "Natural Language :: English",
+    ],
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=["requests", "anchorpoint>=0.5.2", "eyecite>=2.2.0", "pydantic"],
+    python_requires=">=3.8",
+)
