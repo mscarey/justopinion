de8675e8de25b6f94fe62b74bf442ea68d0929ab
Matt Carey <matt@authorityspoke.com>
2024-02-04 01:01:55 -0600

    remove support for Python 3.9

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setuptools.setup(
     url="https://github.com/mscarey/justopinion",
     project_urls={"Bug Tracker": "https://github.com/mscarey/justopinion/issues"},
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
         "License :: Free To Use But Restricted",
         "Natural Language :: English",
